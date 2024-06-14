d9b17fe9d0175cb88b9458a6268e25d331e2d3d5
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-14 12:49:46 -0700

    finished 15 for loops

--- exercise1-15loops/Program.cs
+++ exercise1-15loops/Program.cs
@@ -1,3 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//for(int i = 0; i < 5; i++)
+//{
+    //Console.WriteLine($"{i}");
+//}
 
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+
+for(int i =0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i <5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5;i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i =0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
